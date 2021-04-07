960f19e655969f2829e03e68f2b7268b1b50277b
alallala <64277365+alallala@users.noreply.github.com>
2021-04-07 14:02:07 +0100

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -24,7 +24,15 @@ class DataGenerator(object):
         self.num_samples_per_class = num_samples_per_class #in 1-shot we have just one image per class, in 5 shot we have 5 images per class and so on 
         self.num_classes = 1  # by default 1 (only relevant for classification problems)
         
-        if 'omniglot' in FLAGS.datasource:
+	if FLAGS.datasource == 'sinusoid':
+            self.generate = self.generate_sinusoid_batch
+            self.amp_range = config.get('amp_range', [0.1, 5.0])
+            self.phase_range = config.get('phase_range', [0, np.pi])
+            self.input_range = config.get('input_range', [-5.0, 5.0])
+            self.dim_input = 1
+            self.dim_output = 1
+		
+        elif 'omniglot' in FLAGS.datasource:
             self.num_classes = config.get('num_classes', FLAGS.num_classes)
             self.img_size = config.get('img_size', (28, 28))
             self.dim_input = np.prod(self.img_size)
@@ -208,4 +216,18 @@ class DataGenerator(object):
         all_label_batches = tf.one_hot(all_label_batches, self.num_classes)
         
         return all_image_batches, all_label_batches
+
+def generate_sinusoid_batch(self, train=True, input_idx=None):
+        # Note train arg is not used but it is used for omniglot method.
+        # input_idx is used during qualitative testing --the number of examples used for the grad update
+        amp = np.random.uniform(self.amp_range[0], self.amp_range[1], [self.batch_size])
+        phase = np.random.uniform(self.phase_range[0], self.phase_range[1], [self.batch_size])
+        outputs = np.zeros([self.batch_size, self.num_samples_per_class, self.dim_output])
+        init_inputs = np.zeros([self.batch_size, self.num_samples_per_class, self.dim_input])
+        for func in range(self.batch_size):
+            init_inputs[func] = np.random.uniform(self.input_range[0], self.input_range[1], [self.num_samples_per_class, 1])
+            if input_idx is not None:
+                init_inputs[:,input_idx:,0] = np.linspace(self.input_range[0], self.input_range[1], num=self.num_samples_per_class-input_idx, retstep=False)
+            outputs[func] = amp[func] * np.sin(init_inputs[func]-phase[func])
+        return init_inputs, outputs, amp, phase
 
